39a3b52dc6d0515f06c57cde7ffae5d581507df3
wouldntknow <humnauraizee@gmail.com>
2024-08-15 03:36:01 +0500

    pushing this file to github

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNum = Math.floor(Math.random() * 10 + 1); //will generate numbers from 1-10
+const result = await inquirer.prompt([
+    { message: "Guess a number between 1-10\n", type: "number", name: "userNumber" },
+]);
+if (result.userNumber === randomNum) {
+    console.log("Congratulations, you've guessed the right number!");
+}
+else {
+    console.log("Game failed.Try again!");
+}
